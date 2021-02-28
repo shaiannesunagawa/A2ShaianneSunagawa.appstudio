a1b0afd59e833b0503cf5ec309adf157bd478a0b
shaiannesunagawa <sys74530@creighton.edu>
2021-02-28 14:50:00 -0600

    2.28 2.49

--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -14,20 +14,20 @@ let switchOutput4 = "wear a warm coat, hat and gloves. Maybe a scarf too."
 
 let switchMessages = [switchOutput1, switchOutput2, switchOutput3, switchOutput4]
 
-switch(true) {
-    case 1: (switchTemperature < 32 && switchState == 'NE')
+switch(switchTemperature,switchState) {
+    case 1: (switchTemperature < 32 && switchState === 'NE')
         console.log(`${switchName}, ${switchMessages[0]}`);
         break;
         
-    case 2: ((switchTemperature > 32 && switchTemperature < 50) && switchState == 'NE')
+    case 2: ((switchTemperature > 32 && switchTemperature < 50) && switchState === 'NE')
         console.log(`${switchName}, ${switchMessages[1]}`);
         break;
         
-    case 3: ((switchTemperature > 32 && switchTemperature < 50) && switchState == 'FL')
+    case 3: ((switchTemperature > 32 && switchTemperature < 50) && switchState === 'FL')
         console.log(`${switchName}, ${switchMessages[2]}`);
         break;
         
-    case 4: ((switchTemperature > 50 && switchTemperature < 70) && switchState == 'FL')
+    case 4: ((switchTemperature > 50 && switchTemperature < 70) && switchState === 'FL')
         console.log(`${switchName}, ${switchMessages[3]}`);
         break;
 }
