39aff2a56b105e426c5530048b0e0b927b84df50
shaiannesunagawa <sys74530@creighton.edu>
2021-02-28 14:04:16 -0600

    if-else-working

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -4,22 +4,24 @@ let state = prompt('Enter your state (2 letter abbreviation ie NE or FL)')
 
 let temperature = Number(prompt('Enter the temperature (number only) in Fahrenheit'))
 
-let output1 = "wear a warm coat, hat, scarf and gloves."
+let output1 = "wear a warm coat, hat, scarf and gloves"
 
-let output2 = "wear a warm coat but you won't need a hat, scarf or gloves."
+let output2 = "wear a warm coat but you won't need a hat, scarf or gloves"
 
-let output3 = "wear your warmest coat, a warm hat, a scarf, and warm gloves."
+let output3 = "wear your warmest coat, a warm hat, a scarf, and warm gloves"
 
-let output4 = "wear a warm coat, hat and gloves. Maybe a scarf too."
+let output4 = "wear a warm coat, hat and gloves. Maybe a scarf too"
 
 let messages = [output1, output2, output3, output4]
 
-if (temperature < 32 && state == 'NE')
+if (temperature < 32 && state == 'NE') {
     console.log(`${firstName}, ${messages[0]}`)
-else if ((temperature > 32 || temperature < 50) && state == 'NE')
+} else if ((temperature > 32 && temperature < 50) && state == 'NE') {
     console.log(`${firstName}, ${messages[1]}`)
-else if ((temperature > 32 || temperature < 50) && state == 'FL')
+} else if ((temperature > 32 && temperature < 50) && state == 'FL') {
     console.log(`${firstName}, ${messages[2]}`)
-else if ((temperature > 50 || temperature < 70) && state == 'FL')
-    console.log(`${firstName}, ${messages[3]}`)
+} else if ((temperature > 50 && temperature < 70) && state == 'FL') {
+     console.log(`${firstName}, ${messages[3]}`)
+}    
+
 
---
+++ forms/switch/switch.js
@@ -0,0 +1,32 @@
+/*
+let switchName = prompt('Enter your first name')
+
+let switchState = prompt('Enter your state (2 letter abbreviation ie NE or FL)')
+
+let switchTemperature = Number(prompt('Enter the temperature (number only) in Fahrenheit'))
+
+let switchOutput1 = "wear a warm coat, hat, scarf and gloves."
+
+let switchOutput2 = "wear a warm coat but you won't need a hat, scarf or gloves."
+
+let switchOutput3 = "wear your warmest coat, a warm hat, a scarf, and warm gloves."
+
+let switchOutput4 = "wear a warm coat, hat and gloves. Maybe a scarf too."
+
+let switchMessages = [switchOutput1, switchOutput2, switchOutput3, switchOutput4]
+
+switch(switchTemperature && switchState) {
+    case 1: (switchTemperature < 32 && switchState == 'NE')
+        console.log(`${switchName}, ${switchMessages[0]}`)
+        break;
+    case 2: ((switchTemperature > 32 || switchTemperature < 50) && switchState == 'NE')
+        console.log(`${switchName}, ${switchMessages[1]}`)
+        break;
+    case 3: ((switchTemperature > 32 || switchTemperature < 50) && switchState == 'FL')
+        console.log(`${switchName}, ${switchMessages[2]}`)
+        break;
+    case 4: ((switchTemperature > 50 || switchTemperature < 70) && switchState == 'FL')
+        console.log(`${switchName}, ${switchMessages[3]}`)
+        break;
+}
+*/
